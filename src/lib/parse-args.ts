163ac89fde904b573245e65cf144e7ab6b93b2f1
kauefraga <kauefragarodrigues456@gmail.com>
2022-10-18 18:40:35 -0300

    :fire: remove lib/parse-args

--- src/lib/parse-args.ts
+++
@@ -1,9 +0,0 @@
-type QuickyServerCliArgs = [port: number, path: string];
-
-export function parseArgs(args: string[]): QuickyServerCliArgs {
-  const [port, path] = args.splice(2);
-
-  const portTypeInt = Number(port);
-
-  return [portTypeInt, path];
-}
