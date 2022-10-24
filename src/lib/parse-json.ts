1a6eb495f1c09f3127a141abe1b112d7a3841353
kauefraga <kauefragarodrigues456@gmail.com>
2022-10-23 21:04:14 -0300

    :recycle: chore: add async/await it is a promise

--- src/lib/parse-json.ts
+++ src/lib/parse-json.ts
@@ -1,8 +1,8 @@
 import { resolve } from 'path';
 import { readFile } from 'fs/promises';
 
-export function readJson(path: string) {
-  const json = readFile(resolve(path), 'utf-8')
+export async function readJson(path: string) {
+  const json = await readFile(resolve(path), 'utf-8')
     .then((raw) => JSON.parse(raw));
 
   return json;
