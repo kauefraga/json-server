3421ffafea3aa820325625a6153f321d3db93b6d
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-12-15 20:44:18 -0300

    :recycle: chore: rewrite the console to being able to receive tagged template literal

--- src/lib/colorized-console.ts
+++ src/lib/colorized-console.ts
@@ -1,56 +1,27 @@
-class Log {
-  private colors = {
-    red: '\x1b[31m%s\x1b[0m',
-    green: '\x1b[32m%s\x1b[0m',
-    yellow: '\x1b[33m%s\x1b[0m',
-    blue: '\x1b[34m%s\x1b[0m',
-    magenta: '\x1b[35m%s\x1b[0m',
-    cyan: '\x1b[36m%s\x1b[0m',
-    white: '\x1b[37m%s\x1b[0m',
-    dim: '\x1b[2m%s\x1b[0m',
-  };
-
-  red(message: string) {
-    console.log(this.colors.red, message);
-  }
-
-  green(message: string) {
-    console.log(this.colors.green, message);
-  }
-
-  yellow(message: string) {
-    console.log(this.colors.yellow, message);
-  }
-
-  blue(message: string) {
-    console.log(this.colors.blue, message);
-  }
-
-  magenta(message: string) {
-    console.log(this.colors.magenta, message);
-  }
-
-  cyan(message: string) {
-    console.log(this.colors.cyan, message);
-  }
-
-  white(message: string) {
-    console.log(this.colors.white, message);
-  }
-
-  dim(message: string) {
-    console.log(this.colors.dim, message);
-  }
-
-  error(message: string) {
-    console.error(this.colors.red, message);
-  }
-
-  warn(message: string) {
-    console.warn(this.colors.yellow, message);
-  }
+enum Colors {
+  RESET = '\x1b[0m',
+  RED = '\x1b[31m',
+  GREEN = '\x1b[32m',
+  YELLOW = '\x1b[33m',
+  BLUE = '\x1b[34m',
+  MAGENTA = '\x1b[35m',
+  CYAN = '\x1b[36m',
+  WHITE = '\x1b[37m',
+  DIM = '\x1b[2m',
 }
 
-const log = new Log();
+type Message = TemplateStringsArray | string;
 
-export { log, Log };
+export function warn(message: Message) {
+  console.warn(`${Colors.YELLOW}${message}${Colors.RESET}`);
+}
+
+export function error(message: Message) {
+  console.error(`${Colors.RED}${message}${Colors.RESET}`);
+}
+
+export function tip(message: Message) {
+  console.log(`${Colors.GREEN}${message}${Colors.RESET}`);
+}
+
+export { tip as info };
