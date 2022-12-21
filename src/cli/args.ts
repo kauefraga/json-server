2c4099285f476b730154b622f9a88454d0253014
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-12-21 16:17:02 -0300

    :label: version: update to 2.0.4

--- src/cli/args.ts
+++ src/cli/args.ts
@@ -5,7 +5,7 @@ export const program = new Command();
 program
   .name('Quicky Server')
   .description('A fast json server built with some native libraries and with some mocks. Inspired in json-server.')
-  .version('2.0.3');
+  .version('2.0.4');
 
 interface QuickyServerOptions {
   port: string | number;
