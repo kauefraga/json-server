abc4f86963d2118595ab6bf3680b6c4e8170c3ce
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-12-21 16:16:27 -0300

    :recycle: chore: remove useless conditional

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -3,14 +3,9 @@ export function isUrl(url: string) {
   // eslint-disable-next-line no-useless-escape
   const urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
 
-  if (urlRegEx.test(url)) {
-    return true;
-  }
-
-  return false;
+  return urlRegEx.test(url);
 }
 
-// Verify if the url is secure
 export function isHttps(url: string) {
   const { protocol } = new URL(url);
 
